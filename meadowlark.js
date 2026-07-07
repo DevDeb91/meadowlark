60452e7e789b52e6d00713d179d08bdb32261e9b
Deborah Santucci <deborahsantuccidev@gmail.com>
2026-07-07 23:23:12 +0200

    Chapter 4: added gitignore and moved 'fortune cookie' into module

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
+const fortune = require('./lib/fortune')
 
 const app = express()
 
@@ -9,58 +10,29 @@ app.engine('handlebars', engine({
 }))
 app.set('view engine', 'handlebars')
 
-const fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple"
-]
-
 const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-    // with handlebars
     res.render('home')
-    /* basic displayed text
-    res.type('text/plain')
-    res.send('Meadowlark Travel')
-    */
 })
 
 app.get('/about', (req, res) => {
-    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-    // with handlebars
-    res.render('about', { fortune: randomFortune })
-    /* basic displayed text
-    res.type('text/plain')
-    res.send('About Meadowlark Travel')
-    */
+    res.render('about', { fortune: fortune.getFortune() })
 })
 
 // custom 404 page
 app.use((req, res) => {
     res.status(404)
-    // with handlebars
     res.render('404')
-    /* basic displayed text
-    res.type('text/plain')
-    res.send('404 - Not Found')
-    */
 })
 
 // custom 500 page
 app.use((err, req, res, next) => {
     console.error(err.message)
     res.status(500)
-    // with handlebars
     res.render('500')
-    /* basic displayed text
-    res.type('text/plain')
-    res.send('500 - Server Error')
-    */
 })
 
 app.listen(port, () => console.log(`Express started on http://localhost:${port}; ` + `press Ctrl-C to terminate.`))
