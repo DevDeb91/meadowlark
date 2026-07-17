1175ea553d3e24d6ef75f05be74ad34762a4e982
Deborah Santucci <deborahsantuccidev@gmail.com>
2026-07-17 22:58:16 +0200

    Chapter 8: processing forms

--- meadowlark.js
+++ meadowlark.js
@@ -2,8 +2,12 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const handlers = require('./lib/handlers')
 const weatherMiddleware = require('./lib/middleware/weather')
+const bodyParser = require('body-parser')
+const multiparty = require('multiparty')
 
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Remove the X-Powered-By header for security reasons
 app.disable('x-powered-by')
@@ -30,6 +34,22 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', handlers.home)
 
 app.get('/about', handlers.about)
+
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
+
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+    const form = new multiparty.Form()
+    form.parse(req, (err, fields, files) => {
+        if(err) return res.status(500).send({error: err.message})
+        handlers.vacationPhotoContestProcess(req, res, fields, files)
+    })
+})
+app.get('/contest/vacation-photo-thank-you', handlers.vacationPhotoContestProcessThankYou)
 
 // custom 404 page
 app.use(handlers.notFound)
