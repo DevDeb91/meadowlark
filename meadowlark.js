72bc317aa3f384fc4a935d13f5d00330534f4820
Deborah Santucci <deborahsantuccidev@gmail.com>
2026-07-18 00:36:41 +0200

    Chapter 9: Cookies and Sessions

--- meadowlark.js
+++ meadowlark.js
@@ -4,10 +4,22 @@ const handlers = require('./lib/handlers')
 const weatherMiddleware = require('./lib/middleware/weather')
 const bodyParser = require('body-parser')
 const multiparty = require('multiparty')
+const { credentials } = require('./config')
+const cookieParser = require('cookie-parser')
+const expressSession = require('express-session')
+const flashMiddleware = require('./lib/middleware/flash')
 
 const app = express()
+app.use(cookieParser())
+app.use(expressSession({
+    secret: credentials.cookieSecret,
+    resave: false,
+    saveUninitialized: false
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(flashMiddleware)
 
 // Remove the X-Powered-By header for security reasons
 app.disable('x-powered-by')
@@ -39,7 +51,54 @@ app.get('/newsletter', handlers.newsletter)
 app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
 app.get('/newsletter-signup', handlers.newsletterSignup)
 app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+
+// mock signup "database" object for demonstration purposes;
+// here we would normally save to a real database
+class NewsletterSignup {
+    constructor({ name, email }) {
+        this.name = name
+        this.email = email
+    }
+    save(cb) {
+        cb()
+    }
+}
+
+const VALID_EMAIL_REGEX = new RegExp(
+    '^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$'
+)
+app.post('/newsletter', function(req, res){
+    const name = req.body.name || ''
+    const email = req.body.email || ''
+    // input validation
+    if(!VALID_EMAIL_REGEX.test(email)) {
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Validation error!',
+            message: 'The email address you entered was not valid.'
+        }
+        return res.redirect(303, '/newsletter')
+    }
+
+    new NewsletterSignup({ name, email}).save((err) => {
+        if(err) {
+            req.session.flash = {
+                type: 'danger',
+                intro: 'Database error!',
+                message: 'There was a database error; please try again later.'
+            }
+            return res.redirect(303, '/newsletter/archive')
+        }
+        req.session.flash = {
+            type: 'success',
+            intro: 'Thank you!',
+            message: 'You have now been signed up for the newsletter.'
+        }
+        return res.redirect(303, '/newsletter/archive')
+    })
+})
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
+app.get('/newsletter/archive', handlers.newsletterArchive)
 
 app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
 app.post('/contest/vacation-photo/:year/:month', (req, res) => {
